585934b1fb4ea660810269c9d8742b57fbf0bf2e
Moffat <moffat.muga@gmail.com>
2023-04-02 12:17:50 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName = bob
+
+function upperCaseCustomerName() {
+  var upperCaseName = customerName.toUpperCase();
+  return upperCaseName;
+}
+  
+
+  function setBestCustomer() {
+    bestCustomer = "not bob";
+    return bestCustomer;
+  }
+  
+  console.log(setBestCustomer());
+
+  console.log(overwriteBestCustomer());
+
+  const leastFavoriteCustomer = "Bob";
+
+  function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "John";
+  return leastFavoriteCustomer;
+}
+console.log(changeLeastFavoriteCustomer());    
+
